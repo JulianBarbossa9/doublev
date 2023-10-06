19a0ee91b1f7286ce683ca5a0e1133ea0217e57a
Julian Barbosa <julianbarbossa@hotmail.com>
2023-10-06 09:13:06 -0500

    add react router

--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -2,9 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import ErrorPage from './components/ErrorPage.jsx';
+import UserDetail from './components/UserDetail.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "user/:login",
+    element: <UserDetail />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+     <RouterProvider router={router} />
   </React.StrictMode>,
 )

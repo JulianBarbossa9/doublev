ff69fe162a2aadcbcd6478e0c15e23a2803b2450
Julian Barbosa <julianbarbossa@hotmail.com>
2023-10-06 15:45:20 -0500

    update app and formated

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,29 +1,15 @@
 import { useEffect, useState } from 'react'
-import './App.css'
 import axios from 'axios'
+import SearchComponent from './components/SearchComponent'
 
 function App() {
-  const [ data, setData ] = useState("")
-
-  const makeApiCall = async () => {
-    try {
-      const response = await fetch('http://localhost:3001/api')
-      const data = await response.json()
-      console.log({data})
-      setData(data.message)
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  useEffect(() => {
-    makeApiCall()
-  },[])
+  
 
   return (
     <>
-     <div>
-        <h3>{data}</h3>
+     <div className='flex flex-col justify-center align-middle text-center mt-8'>
+        <h3 className='text-4xl font-bold underline'>Users of GitHub</h3>
+        <SearchComponent />
      </div>
     </>
   )
